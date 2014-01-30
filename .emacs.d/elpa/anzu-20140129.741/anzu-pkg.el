fd81c8f3bdcfc4bc4b47694ecf8c49a789c408d0
Manu Campos <manueltxo@gmail.com>
2014-01-30 18:16:37 +0100

    Adds emacs config

---
+++ .emacs.d/elpa/anzu-20140129.741/anzu-pkg.el
@@ -0,0 +1,1 @@
+(define-package "anzu" "20140129.741" "Show number of matches in mode-line while searching" 'nil)
---
+++ .emacs.d/elpa/flx-20130728.820/flx-pkg.el
@@ -0,0 +1,1 @@
+(define-package "flx" "20130728.820" "fuzzy matching with good sorting" '((cl-lib "0.3")))
